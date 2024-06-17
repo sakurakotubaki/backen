50a82cb526855f589e2026fbac0f26ee74ffa1dc
Jboy422 <gitonly543@gmail.com>
2024-06-17 16:14:59 +0900

    add: docker-compose

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Task } from './models/task.model';
 import { TaskService } from './task.service';
+import { CreateTaskInput } from './dto/createTask.input';
 
 @Resolver()
 export class TaskResolver {
@@ -13,10 +14,11 @@ export class TaskResolver {
 
   @Mutation(() => Task)
   createTask(
-    @Args('name') name: string,
-    @Args('dueDate') dueDate: string,
-    @Args('description', { nullable: true }) description?: string,
-  ): Task {
-    return this.taskService.createTask(name, dueDate, description);
+    // @Args('name') name: string,
+    // @Args('dueDate') dueDate: string,
+    // @Args('description', { nullable: true }) description?: string,
+    @Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
+    // return this.taskService.createTask(name÷, dueDate, description);
+    return this.taskService.createTask(createTaskInput);
   }
 }
